6f4b8ab5f71d35798547fa05e7ffbaa86bcbd842
Kenyatta Hill <khill@insight2profit.com>
2020-04-12 23:26:38 -0400

    Ui is built

---
+++ index.js
@@ -0,0 +1,7 @@
+const forcars_url =
+  'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}';
+
+const current_weather =
+  'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}';
+
+  const key = '4f7b09b9612d5cab9d797eef0e3e4c02';
